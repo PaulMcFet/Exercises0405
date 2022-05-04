4e5327e9792704a316403d0bcb111698edbb0473
Paul <paulmcfetridge1993@gmail.com>
2022-05-04 11:33:10 +0100

    PM: Adding updates so far

--- Exercise0405/calculator.js
+++ Exercise0405/calculator.js
@@ -1,3 +1,25 @@
 module.exports.add = function(x, y) {
     return x+y; 
 }
+
+module.exports.subtract = function(x, y) {
+    return x-y; 
+}
+
+module.exports.multiply = function(x, y) {
+    return x*y; 
+}
+
+module.exports.divide = function(x, y) {
+    return x/y; 
+}
+
+module.exports.iseven = function(x){
+    if (x%2 == 0);
+    return true; 
+}
+
+module.exports.isodd = function(x){
+    if (x%2 !== 0);
+    return true; 
+}
--- Exercise0405/index.js
+++ Exercise0405/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { print } = require('../my-first-express-app/my_js_modules/helper');
 const calculator = require('./calculator')
 
 const port = process.env.PORT || 3000;
@@ -10,6 +11,34 @@ app.get("/add", (request, response) => {
     const x = parseInt(request.query.x);
     const y = parseInt(request.query.y);
     response.status("200").send(calculator.add(x,y).toString());
+});
+
+app.get("/subtract", (request, response) => {
+    const x = parseInt(request.query.x);
+    const y = parseInt(request.query.y);
+    response.status("200").send(calculator.subtract(x,y).toString());
+});
+
+app.get("/multiply", (request, response) => {
+    const x = parseInt(request.query.x);
+    const y = parseInt(request.query.y);
+    response.status("200").send(calculator.multiply(x,y).toString());
+});
+
+app.get("/divide", (request, response) => {
+    const x = parseInt(request.query.x);
+    const y = parseInt(request.query.y);
+    response.status("200").send(calculator.divide(x,y).toString());
+});
+
+app.get("/iseven", (request, response) => {
+    const x = parseInt(request.query.x);
+    response.status("200").send(calculator.iseven(x).toString());
+});
+
+app.get("/isodd", (request, response) => {
+    const x = parseInt(request.query.x);
+    response.status("200").send(calculator.isodd(x).toString());
 });
 
 const server = app.listen(port, () => {
